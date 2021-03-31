b7422588314ff15991f1bc7b66dc12931dadadb6
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-03-31 20:28:22 +0000

    cleaned up code

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
-    @products = Product.order(id: :desc).all
+    @products = Product.all
     @category = Category.all;
   end
 
