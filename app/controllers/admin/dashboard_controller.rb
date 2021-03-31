0487aff7ef46139c9cefd875a5e24a6eaf0569e5
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-03-31 20:27:27 +0000

    added Dashboard element for Admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @products = Product.order(id: :desc).all
+    @category = Category.all;
   end
+
 end
