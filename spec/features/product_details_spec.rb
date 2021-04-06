68c76393760c713005b7f65e4f90d1fcf322f684
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-06 15:23:45 +0000

    added test to navigate into product page

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -4,26 +4,23 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
-
-    10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
+    @category.products.create!(
+        name:  'Hat',
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
-    end
   end
 
-
-  scenario "They see all products" do
+  scenario "Navigates into Product" do
     # ACT
     visit root_path
+    click_on 'Details'
 
     # DEBUG / VERIFY
+    expect(page).to have_text 'Quantity'
     save_screenshot
-    expect(page).to have_css 'article.product', count: 10
   end
 
 end
