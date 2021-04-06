f7c811293cd658dd8960cfe8c8da90ed68352954
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-04-06 18:34:14 +0000

    Fixed test to be a little more dynamic

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -4,19 +4,22 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
   before :each do
     @category = Category.create! name: 'Apparel'
-    @category.products.create!(
-        name:  'Hat',
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
         image: open_asset('apparel1.jpg'),
         quantity: 10,
         price: 64.99
       )
+    end
   end
 
   scenario "Adds a product to the cart" do
     # ACT
     visit root_path
-    click_on 'Add'
+    first('article.product').click_on 'Add'
 
     # DEBUG / VERIFY
     expect(page).to have_text 'My Cart (1)'
